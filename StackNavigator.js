df3bf75c97b7c45dbdc2b9d55c5a01b64f6d2dac
Aashish Karki <078bct004.aashish@pcampus.edu.np>
2024-02-15 20:58:49 +0545

    Stack Navigator Setup

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,12 +1,19 @@
-import {createBottomTabNavigator} from 'react-navigation-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./screens/HomeScreen";
 
-const Tab = createBottomTabNavigator();
-
-function BottomTabs(){
-    return (
-        <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-        </Tab.Navigator>
-    );
+const Stack = createNativeStackNavigator();
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
+        {/* <Stack.Screen name="Liked" component={LikedSongsScreen} options={{headerShown:false}}/> 
+                <Stack.Screen name="Info" component={SongInfoScreen} options={{headerShown:false}}/> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+export default Navigation;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,22 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import {LinearGradient} from 'expo-linear-gradient';
+import React from "react";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaViewView>
-      <Text>HomeScreen</Text>
-    </SafeAreaViewView>
-  )
-}
+    <LinearGradient
+      colors={["#040306", "#131624"]}
+      style={{ flex: 1 }}
+    >
+      <ScrollView style={{marginTop: 50}}>
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <Text style={{ color: "white", fontSize: 30 }}>Home Screen</Text>
+        </View>
+      </ScrollView>
+    </LinearGradient>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
